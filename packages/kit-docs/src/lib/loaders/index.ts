eef364b6762c64dd4b8da26717542851f5441e67
Rahim Alwer <rahim.alwer@gmail.com>
2022-04-18 01:00:06 +1000

    feat: sidebar loader can now accept multi-path config object

--- packages/kit-docs/src/lib/loaders/index.ts
+++ packages/kit-docs/src/lib/loaders/index.ts
@@ -1,7 +1,9 @@
 import type { Load } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 
 import type { ResolvedSidebarConfig } from '$lib/components/layout/contexts.js';
 import type { MarkdownMeta } from '$lib/stores/kitDocs.js';
+import { isString } from '$lib/utils/unit';
 
 export function getRootDirFromUrl(url: URL) {
   return url.pathname.split('/')[1];
@@ -15,14 +17,16 @@ export function normalizePath(path: string) {
   return path.replace(/^\//, '').replace(/\/$/, '');
 }
 
+export type LoaderFetchFn = (info: RequestInfo, init?: RequestInit) => Promise<Response>;
+
 /**
  * @param slug - A slug that will be resolved to a markdown file from which a meta object will be
  * built (e.g., `docs/introduction`).
- * @param fetch - SvelteKit fetch function.
+ * @param input - SvelteKit loader input.
  */
 export async function loadKitDocsMeta(
   slug: string,
-  fetch: (info: RequestInfo, init?: RequestInit) => Promise<Response>,
+  { fetch }: { fetch: LoaderFetchFn },
 ): Promise<MarkdownMeta | null> {
   try {
     const res = await fetch(
@@ -36,39 +40,75 @@ export async function loadKitDocsMeta(
   }
 }
 
+export type SidebarLoaderPath = string | { [path: string]: string };
+
 /**
  * @param path - A path relative to the `routes/` directory. Markdown files will be resolved from
- * the matched directory and used to build a sidebar config object (e.g., `/docs`).
- * @param fetch - SvelteKit fetch function.
+ * the matched directory and used to build a sidebar config object (e.g., `/docs`). You can also
+ * provide a multi-path configuration that will resolve the directory to the key that matches the
+ * current path (e..g, `{ '/': null, '/docs': '/docs' }`).
+ * @param input - SvelteKit loader input.
  */
 export async function loadKitDocsSidebar(
-  path: string,
-  fetch: (info: RequestInfo, init?: RequestInit) => Promise<Response>,
+  path: SidebarLoaderPath,
+  { url, fetch }: { url: URL; fetch: LoaderFetchFn },
 ): Promise<ResolvedSidebarConfig | null> {
+  const matchedPath = matchSidebarPath(url, path);
+
+  if (!matchedPath) {
+    return null;
+  }
+
   try {
-    const res = await fetch(`/kit-docs/${slugToRequestParam(path)}.sidebar.json`);
+    const res = await fetch(`/kit-docs/${slugToRequestParam(matchedPath)}.sidebar.json`);
     return res.json();
   } catch (e) {
     return null;
   }
 }
 
+export function matchSidebarPath(url: URL, path: SidebarLoaderPath): string | null {
+  if (isString(path)) return path;
+
+  const currentPath = url.pathname;
+  // Match deep paths first.
+  const sortedPaths = Object.keys(path).sort((a, b) => b.length - a.length);
+
+  for (const possiblePath of sortedPaths) {
+    if (currentPath.startsWith(possiblePath)) {
+      return path[possiblePath];
+    }
+  }
+
+  return null;
+}
+
 export type KitDocsLoaderOptions = {
   /**
    * A path relative to the `routes/` directory. Markdown files will be resolved from the
-   * matched directory and used to build a sidebar config object.
+   * matched directory and used to build a sidebar config object. You can also provide a
+   * multi-path configuration that will resolve the directory to the key that matches the current
+   * path.
    *
    * @example `/docs`
+   * @example
+   * ```js
+   * {
+   *   '/': null,
+   *   '/docs': '/docs',
+   *   '/faq': '/faq',
+   * }
+   * ```
    */
-  sidebar?: string;
+  sidebar?: SidebarLoaderPath;
 };
 
 export function createKitDocsLoader(options: KitDocsLoaderOptions = {}): Load {
   return async ({ url, fetch }): Promise<LoadKitDocsResult> => {
-    const meta = await loadKitDocsMeta(url.pathname, fetch);
+    const meta = await loadKitDocsMeta(url.pathname, { fetch });
     return {
       props: options.sidebar
-        ? { meta, sidebar: await loadKitDocsSidebar(options.sidebar, fetch) }
+        ? { meta, sidebar: await loadKitDocsSidebar(options.sidebar, { url, fetch }) }
         : { meta },
     };
   };
--- packages/kit-docs/src/routes/__layout-kit-docs.svelte
+++ packages/kit-docs/src/routes/__layout-kit-docs.svelte
@@ -1,7 +1,12 @@
 <script context="module">
   export const prerender = true;
 
-  export const load = createKitDocsLoader({ sidebar: '/docs' });
+  export const load = createKitDocsLoader({
+    sidebar: {
+      '/': null,
+      '/docs': '/docs',
+    },
+  });
 </script>
 
 <script>
