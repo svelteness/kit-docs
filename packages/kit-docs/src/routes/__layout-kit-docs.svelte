798e340f281dd2948f109b02742b87386661f794
Rahim Alwer <rahim.alwer@gmail.com>
2022-04-25 11:31:14 +1000

    fix: layout not set correctly when navbar is `false`

--- packages/kit-docs/src/routes/__layout-kit-docs.svelte
+++ packages/kit-docs/src/routes/__layout-kit-docs.svelte
@@ -73,7 +73,7 @@
 </svelte:head>
 
 <KitDocs {meta}>
-  <KitDocsLayout navbar={false} {sidebar}>
+  <KitDocsLayout {navbar} {sidebar}>
     <div slot="navbar-left">
       <div class="logo">
         <Button href="/docs">
