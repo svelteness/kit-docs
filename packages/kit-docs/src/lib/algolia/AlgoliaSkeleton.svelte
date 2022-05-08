55e26a36ea43b9d9fb2d2825b3fa8c17ca22abc9
Rahim Alwer <rahim.alwer@gmail.com>
2022-05-08 14:06:02 +1000

    fix: slight docsearch styling improvements

--- packages/kit-docs/src/lib/algolia/AlgoliaSkeleton.svelte
+++ packages/kit-docs/src/lib/algolia/AlgoliaSkeleton.svelte
@@ -6,7 +6,20 @@
 <div
   class="hidden w-full items-center rounded-sm border border-gray-divider bg-gray-elevate py-2.5 px-3 text-[15px] shadow-sm 992:flex"
 >
-  <span class="flex-1">Search</span>
+  <div class="flex-1 flex items-center">
+    <svg width="20" height="20" class="DocSearch-Search-Icon" viewBox="0 0 20 20">
+      <path
+        d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+        stroke="currentColor"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <span>Search</span>
+    </svg>
+  </div>
+
   <span class="flex space-x-0.5 font-semibold">
     <span>⌘</span>
     <span>K</span>
