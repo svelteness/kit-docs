5a399b13debc48741cf93c56057b87af0c909586
Rahim Alwer <rahim.alwer@gmail.com>
2022-07-13 10:18:07 +1000

    chore: use relative paths in `tsconfig`

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import icons from 'unplugin-icons/vite';
 import kitDocs from '@svelteness/kit-docs/node';
+import icons from 'unplugin-icons/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
