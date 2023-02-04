6ce082f311c85294d4639a6c48144b3923a4778a
Rahim Alwer <rahim.alwer@gmail.com>
2023-02-04 17:20:33 +1100

    fix: footer should only accept default slot

--- packages/kit-docs/src/lib/components/layout/Footer.svelte
+++ packages/kit-docs/src/lib/components/layout/Footer.svelte
@@ -1,11 +1,5 @@
 <footer
   class="992:mt-32 mt-20 flex w-full flex-col items-center justify-center border-t border-border py-12 text-center text-base font-medium"
 >
-  <slot name="footer-top" />
-
-  <span class="text-soft mt-8">
-    © Vidstack {new Date().getFullYear()}
-  </span>
-
-  <slot name="footer-bottom" />
+  <slot />
 </footer>
