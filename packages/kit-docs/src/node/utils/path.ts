d6b4cd813d1f9ecb61ac63c0321cf703ba246731
Gabriel d Agosto <gabriel.dagosto@engenharia.ufjf.br>
2023-03-11 14:56:55 -0300

    fix: normalize windows paths to slashes

--- packages/kit-docs/src/node/utils/path.ts
+++ packages/kit-docs/src/node/utils/path.ts
@@ -3,3 +3,7 @@ import { basename, extname } from 'path';
 export function getFileNameFromPath(filePath: string) {
   return basename(filePath, extname(filePath));
 }
+
+export function slash(str: string) {
+  return str.replace(/\\/g, '/');
+}
