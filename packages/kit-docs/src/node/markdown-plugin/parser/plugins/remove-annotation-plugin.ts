75aad903425ebe75e397400acb8e7aecd16606f3
Gabriel d`Agosto <gabriel.dagosto@engenharia.ufjf.br>
2023-11-27 17:35:06 -0300

    refactor: dont remove annotations, use @html

--- packages/kit-docs/src/node/markdown-plugin/parser/plugins/remove-annotation-plugin.ts
+++ packages/kit-docs/src/node/markdown-plugin/parser/plugins/remove-annotation-plugin.ts
@@ -1,5 +1,5 @@
 import { type PluginSimple } from 'markdown-it';
-const annotationRe = /<annotation(.|\n)*?<\/annotation>/g;
+const annotationRe = /(<annotation[\s\S]+?>)([\s\S]+?)(<\/annotation>)/g;
 
 /**
  * Resolves link URLs.
@@ -10,7 +10,22 @@ export const removeAnnotationPlugin: PluginSimple = (parser) => {
     if (!fn) return;
 
     // Fix math rules, to remove annotation blocks afterward
-    parser.renderer.rules[name] = (tokens, idx, options, env, renderer) =>
-      fn(tokens, idx, options, env, renderer).replace(annotationRe, '');
+    parser.renderer.rules[name] = (tokens, idx, options, env, renderer) => {
+      let result = fn(tokens, idx, options, env, renderer);
+
+      [...result.matchAll(annotationRe)].forEach((match) => {
+        /* Given an annotation rule with a backslash or curly braces, modify it using svelte's @html 
+           to avoid svelte attempting to parse variables, or processing backslashes
+           
+           Input: <annotation>\rule {braces}</annotation>
+           Output: <annotation>{@html `\\rule {braces}`}<annotation> */
+        result = result.replace(
+          match[0],
+          `${match[1]}{@html \`${match[2].replace('\\', '\\\\')}\`}${match[3]}`,
+        );
+      });
+
+      return result;
+    };
   });
 };
